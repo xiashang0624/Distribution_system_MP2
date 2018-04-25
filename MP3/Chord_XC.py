2c54028b2aa9d9465b003af05642304c4d0c1db4
xyu335 <leonyu365@gmail.com>
2018-04-25 17:39:11 -0500

    response+request, not debugged

--- MP3/Chord_XC.py
+++ MP3/Chord_XC.py
@@ -10,12 +10,14 @@ import pdb
 # 3. client_execute(), execute the command from buffer one by one
 # 4. if node_n joined later,  node_n.server() should run all the time before it is crashed.
 
-operation_buffer = []
+Command_buff = []
+receive_buffer = []
 '''
     ch_node represents a peer in the network
 '''
 
-
+global flag
+# *************** Listen and responding ****************
 # following part is replica_server to listen any operations for modifying the replica
 def Node_listen(node_socket):
     while True:
@@ -23,41 +25,57 @@ def Node_listen(node_socket):
             recieve_msg,addr = node_socket.recvfrom(1024)
             receive_src = addr[-1] % 100
             receive_str = recieve_msg.decode('utf-8')
-            msg = receive_str.split(',')
+            msg = receive_str.split()
             operata = msg[0]
             # recv_time = time.asctime().strip().split()[3]
-            #if operata == 'write':
-            #    replica_update(msg[1], int(msg[2]), msg[3], receive_src)
+            if operata == 'find_response':
+                receive_buffer.append(receive_str)
 
+            if operata == 'find_predecessor':
+                msg = 'find_predecessor ' + str(id) + ' ' + str(P_ID) + ' ' + addr_lookup[P_ID]
+                find_response(int(msg[1]), int(msg[2]), msg[3])
         except:
             pass
 
 
+def find_response(id, request_node_id, request_node_addr):
+    dest_id, dest_addr = find_predecessor(id, request_node_id, request_node_addr)
+    if dest_id == P_ID:
+        msg = 'find_response' + str(dest_id)+' ' + str(dest_addr)
+        Unicast(s, request_node_addr, msg)
 
 
-
-
-
-
-def client_input():
+# ******************* Client ****************
+# Client input: depending on the input format,
+# delay function embedded.
+def Client_input():
     while True:
-        message = input('input your command:\n')
+        message = input('Client: enter command here:\n')
         msg = message.split()
-        if msg[0] == 'exit':
-            print('manually exit ')
-            exit()
-        elif msg[0] in {'crash',  'join', 'show'}:
-            operation_buffer.append(msg)
+        if not msg:
+            print('Error input, input should use the following format: find/join/crash + node + (key)')
+            pass
         else:
-            print('Invalid input')
+            command= msg[0]
+            if command == 'get' and len(msg)==2 and msg[1] in share_V: # write the get command to log file and broadcast
+                Command_buff.put(message)
+            elif command == 'put' and len(msg) == 3 and msg[1] in share_V:          # write the put command to log file and broadcast
+                Command_buff.put(message)
+            elif command == 'delay' and len(msg) == 2:        # put the stdin sleep
+                Command_buff.put(message)
+            elif command == 'dump' and len(msg) == 1:    # print all the key-value pairs in shared memory
+                Command_buff.put(message)
+            else:
+                print('Error input, input should use the following format: put/get/delay/dump + (key + value).')
+                pass
 
 
 def client_execute(s):
-    input_thread = threading.Thread(target=(client_input()), args=(s))
+    input_thread = threading.Thread(target=Client_input, args=(s))
     input_thread.start()
     while True:
-        if len(operation_buffer) != 0:
-            msg = operation_buffer.pop(0)
+        if len(Command_buff) != 0:
+            msg = Command_buff.pop(0)
             if msg[0] == 'crash':
                 Unicast(s, msg[1], msg[0])
             elif msg[0] == 'show':
@@ -77,7 +95,6 @@ def client_execute(s):
     # req_time = time.asctime().split()[3].replace(':', '')
     # print('dump ' + req_time)
 
-
 # ****************broadcast with delay***************
 # define a function to uni-cast
 def Unicast(client_socket, target, message):
@@ -92,6 +109,38 @@ def Delay(client_socket, target, message):
     time.sleep(delay_time)
     client_socket.sendto(message.encode('utf-8'), target)
 
+
+# ***************Chrod functions ********************************
+# find the succ and pred
+def find_successor(id):
+    n_id, n_addr = find_predecessor(id, P_ID, addr_lookup[P_ID])
+    return n_id, n_addr
+
+
+def find_predecessor(id, src_id, src_addr):
+    n_id, n_addr= P_ID, addr_list[P_ID]
+    if not (n_id < id and n_id >= FT_succ[0]):
+        # to verify if it is between two consecutive node
+        n_id, n_addr = closest_preceding_finger(id)
+        msg = 'find_predecessor '+str(id) + ' ' + str(src_id) + ' ' + src_addr
+        Unicast(s, n_addr, msg)
+        flag = True
+        while(flag):
+            time.sleep(0.05)
+        n_id, n_addr = receive_buffer.pop()
+    if src_id == P_ID:
+        return n_id, n_addr
+    else:
+        response = 'find_response ' + str(n_id) + ' ' + str(n_addr)
+        Unicast(s, src_addr, response)
+
+
+def closest_preceding_finger(id):
+    for i in range(7,-1):
+        if FT_succ[i] > P_ID and FT_succ[i] < id:
+            # gradually get close to the closest point
+            return FT_succ[i], addr_lookup[FT_succ[i]]
+    return P_ID, addr_lookup[P_ID]
 
 
 # **********Main**************
@@ -133,9 +182,6 @@ while P_ID not in PID_pool:
 
 print('The process number selected is: {}'.format(P_ID))
 
-
-pdb.set_trace()
-
 # Bind node to a socket and use one thread to listen messages
 identifier = P_ID
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -144,42 +190,61 @@ listen_thread = threading.Thread(target=Node_listen, args=(s,))
 listen_thread.start()
 
 
-# find the succ and pred
-def find_successor(id):
-    n_id, n_addr = find_predecessor(id)
-    return
-
-
-def find_predecessor(id):
-    n_id, n_addr= P_ID, addr_list[P_ID]
-    if not (n_id < id and n_id > FT_succ[0]):
-        n_id, n_addr = closet_preceding_finger(id)
-    # else:
-        # msg = 'find_pred,'+ str(id)
-        # Unicast(client_socket, n_id, msg)
-
-    return n_id, n_addr
-
-
-def closet_preceding_finger(id):
-    for i in range(7,-1):
-        if FT_succ[i] > P_ID and FT_succ[i] < id:
-            return FT_succ[i],
-    return n_id, n_addr
-
-
 # Initilize finger table and Keys
 FT_start = []
 FT_succ = []
 keys = []
 
+FT_interval = [] # function as 'mod' to finger table
+
+addr_lookup = {}
+addr_lookup[0] = addr_list[0]
+addr_lookup[1] = addr_list[1]
+addr_lookup[100] = addr_list[2]
+addr_lookup[200] = addr_list[3]
+
 if P_ID == 0:
     for i in range(8):
         FT_start.append(P_ID + 2**i)
-    FT_succ
+    Node_ID = 0
+    FT_succ=[1,100,100,100,100,100,100,200]
+    keys.append(0)
+    for i in range(201,256):
+        keys.append(i)
+
+elif P_ID ==1:
+    for i in range(8):
+        FT_start.append(P_ID + 2**i)
+    Node_ID = 1
+    FT_succ=[100,100,100,100,100,100,100,200]
+    keys.append(1)
+
+elif P_ID ==2:
+    for i in range(8):
+        FT_start.append(100 + 2**i)
+    Node_ID = 100
+    FT_succ=[200,200,200,200,200,200,200,0]
+    for i in range(2,101):
+        keys.append(i)
+
+elif P_ID ==3:
+    for i in range(8):
+        FT_start.append((200 + 2**i)%2**8)
+    Node_ID = 200
+    FT_succ=[0,0,0,0,0,0,100,100]
+    for i in range(101,201):
+        keys.append(i)
 
 
-# bind socket to the ip address based on the config file
-client_socket= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.bind(addr_list[-1])
-client_execute(client_socket)
+print ('\nFT_start for P_ID %2d:'%Node_ID)
+print (FT_start)
+print ('\nFT_succ for P_ID %2d:'%Node_ID)
+print (FT_succ)
+print ('\nKeys for P_ID %2d:\n'%Node_ID)
+print (keys)
+
+
+
+# the main program is used for clinet to take input message and process it
+Client_input()
+
